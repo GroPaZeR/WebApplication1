43675eb96c86869a3400dcc14f7dccd95cf4f625
Dmitry Mukharski <dmitry9678@gmail.com>
2026-06-12 23:51:11 +0300

    сделано нормальное оттображение текста в таблицах. в базу данных добавлена картинка для каждой статьи

--- WebApplication1/Pages/OneTheme.cshtml.cs
+++ WebApplication1/Pages/OneTheme.cshtml.cs
@@ -1,3 +1,4 @@
+using ClassLibrary;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,9 +6,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 public class OneThemeModel : PageModel
 {
     public string Theme {  get; set; }
+    public Article[] Articles { get; set; }
     public void OnGet(string category)
     {
         Theme = category;
-        Console.WriteLine(1);
+        Articles = GetTwelfeArticles();
+    }
+
+    private Article[] GetTwelfeArticles()
+    {
+        Article[] twelfeArticles = new Article[12];
+
+        News news = new News();
+        news.ReadArticles();
+
+        for (int i = 0, j = 0; i < news.articles.Count; i++)
+        {
+            if (news.articles[i].theme == Theme)
+            {
+                twelfeArticles[j] = news.articles[i];
+                j++;
+            }
+        }
+
+        return twelfeArticles;
     }
 }
